084b5c26ae418b0c8a789c9d58e351d5b019e0cb
Glyaleth <thomas.diggs97@gmail.com>
2020-03-09 20:04:35 -0500

    Added mySQL to the Java program. Updated the AbstractEntity.java file to validate input to not be blank. Updated Employer.java to have a getter and setter for location.

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,19 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.validation.constraints.NotBlank;
+
 public class Employer extends AbstractEntity {
+
+    @NotBlank
+    private String location;
+
+    public String getLocation(){
+        return this.location;
+    };
+    
+    public void setLocation(String location){
+      this.location = location;
+    };
 
 
 }
